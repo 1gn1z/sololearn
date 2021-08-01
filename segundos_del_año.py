563542cee9b881dac61f7b504a4879d3e4b551a7
1gn1z_el_angel <kebor327@gmail.com>
2021-08-01 00:48:04 -0500

    ex2. v2 programa simplificado

--- segundos_del_año.py
+++ segundos_del_año.py
@@ -8,3 +8,10 @@ print(minutos_mes)
 
 segundos_mes = minutos_mes * 60
 print(segundos_mes)
+
+# Vamos a tratar de simplificar el codigo:
+
+horas_mes1 = 24 * 30 * 60 * 60
+#      24 horas * 30 dias = 720horas
+# Ya que sabemos que el mes tiene 720 horas, podemos multiplicar 720 horas * 60 minutos, y el resultado (que son los minutos totales minutos) y luego * 60 que son los segundos                                         
+print(horas_mes1)
