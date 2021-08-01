e326e72bfea9937ad31b18c305db5ffb3f78eaa5
1gn1z_el_angel <kebor327@gmail.com>
2021-08-01 00:42:06 -0500

    ex1. v1 del programa

---
+++ segundos_del_año.py
@@ -0,0 +1,10 @@
+# Calcular los segundos de un mes (30 dias)
+
+horas_mes = 24 * 30
+print(horas_mes)
+
+minutos_mes = horas_mes * 60
+print(minutos_mes)
+
+segundos_mes = minutos_mes * 60
+print(segundos_mes)
